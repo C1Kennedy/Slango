d2d1e2101cec59acd44d36d7fc71dc5af7d5a5b7
blankbruno <jrbruno@miners.utep.edu>
2023-11-06 15:57:30 -0700

    "buggy shit"

--- slango_model.py
+++ slango_model.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
+from collections import Counter
 import spacy
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 max_words = 10000
 
 df = pd.read_csv('urbandict-word-defs.csv', error_bad_lines=False, nrows=max_words)
 
-df.dropna()
+df.dropna(inplace = True)
 df.drop_duplicates(subset=['word', 'definition'], inplace = True)
 
 dataset = df[['word', 'up_votes', 'down_votes', 'definition']].copy()
@@ -37,6 +39,18 @@ dataset['tokens'] = dataset['definition'].apply(tokenize_text)
 definitions = dataset['tokens']
 words = dataset['word']
 
+UNK_TOKEN = '<UNK>'
+word_to_index = {word: index for index, word in enumerate(words)}
+word_to_index[UNK_TOKEN] = len(words)  # Add UNK token to the vocabulary
+index_to_word = {index: word for index, word in enumerate(words)}
+index_to_word[len(words)] = UNK_TOKEN  # Update the index-to-word mapping
+
+def tokens_to_indices(tokens):
+    return [word_to_index.get(token, word_to_index[UNK_TOKEN]) for token in tokens]
+
+# Apply the conversion to dataset
+definitions_tokens_indices = definitions.apply(tokens_to_indices)
+
 class TextGenerationDataset(Dataset):
     def __init__(self, definitions, words, max_sequence_length):
         self.definitions = definitions
@@ -49,12 +63,14 @@ class TextGenerationDataset(Dataset):
     def __getitem__(self, index):
         definitions_tokens = self.definitions.iloc[index]
         word_index = self.words.iloc[index]
+
+        word_index = word_to_index[word_index]
         return{
-            'defintion_tokens': definitions_tokens,
+            'definition_tokens': definitions_tokens,
             'word_index': word_index
         }
     
-text_generation_dataset = TextGenerationDataset(definitions, words, max_sequence_length = 20)
+text_generation_dataset = TextGenerationDataset(definitions_tokens_indices, words, max_sequence_length = 20)
 
 class TextGenerationModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim):
@@ -69,7 +85,7 @@ class TextGenerationModel(nn.Module):
         x = self.fc(x)
         return x
     
-vocab_size = len(words)
+vocab_size = len(index_to_word)
 embedding_dim = 128
 hidden_dim = 256
 
@@ -77,7 +93,21 @@ model = TextGenerationModel(vocab_size, embedding_dim, hidden_dim)
 optimizer = optim.Adam(model.parameters(), lr = 0.001)
 criterion = nn.CrossEntropyLoss()
 
-data_loader = DataLoader(text_generation_dataset, batch_size = 64, shuffle = True)
+def my_collate_fn(batch):
+    definitions_tokens = [item['definition_tokens'] for item in batch]
+    word_indices = [item['word_index'] for item in batch]
+    
+    # Pad the sequences to have the same length
+    definitions_tokens_padded = pad_sequence([torch.LongTensor(seq) for seq in definitions_tokens], batch_first=True)
+    word_indices = torch.LongTensor(word_indices)
+    
+    return {
+        'definition_tokens': definitions_tokens_padded,
+        'word_index': word_indices
+    }
+
+
+data_loader = DataLoader(text_generation_dataset, batch_size = 64, shuffle = True, collate_fn=my_collate_fn)
 
 num_epochs = 10
 
@@ -85,14 +115,16 @@ for epoch in range(num_epochs):
     total_loss = 0
     for batch in data_loader:
         definition_tokens = batch['definition_tokens']
-        word_index = batch['word-index']
+        word_index = batch['word_index']
+        word_index = word_index.view(-1, 1)
 
         optimizer.zero_grad()
 
         output = model(definition_tokens)
-        output = output.view(-1, vocab_size)
+        output = output.view(definition_tokens.size(0), -1, vocab_size)
 
-        loss = criterion(output, word_index)
+        # Flatten both the output and word_index tensors to calculate the loss
+        loss = criterion(output.view(-1, vocab_size), word_index.view(-1))
         loss.backward()
         optimizer.step()
 
@@ -100,20 +132,18 @@ for epoch in range(num_epochs):
     
     print(f"Epoch {epoch + 1}, Loss: {total_loss / len(data_loader)}")
 
-def generate_text(model, definition_tokens, max_length = 20):
+def generate_text(model, definition_tokens, max_length=20):
     model.eval()
-    generated_sequence = definition_tokens
+    generated_sequence = definition_tokens.tolist()  # Convert to a Python list
 
     for _ in range(max_length):
-        input_sequence = torch.LongTensor([definition_tokens])
+        input_sequence = torch.LongTensor([generated_sequence[-1]])  # Use the generated sequence
         output = model(input_sequence)
         output = output.squeeze(0)
         predicted_word_index = torch.argmax(output).item()
 
-        generated_sequence.append([predicted_word_index])
-        definition_tokens.append(predicted_word_index)
+        generated_sequence.append(predicted_word_index)
 
-    generated_text = " ".join([definition_tokens[idx] for idx in generated_sequence])
+    generated_text = " ".join([index_to_word.get(idx, UNK_TOKEN) for idx in generated_sequence])
 
     return generated_text
-
