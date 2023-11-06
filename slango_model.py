e637b35bd13327e2f670342d162c6c290d2d7b22
blankbruno <jrbruno@miners.utep.edu>
2023-11-06 14:21:26 -0700

    "trying pytorch"

--- slango_model.py
+++ slango_model.py
@@ -58,7 +58,7 @@ text_generation_dataset = TextGenerationDataset(definitions, words, max_sequence
 
 class TextGenerationModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim):
-        super(TextGenerationModel, self).__init()
+        super(TextGenerationModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.rnn = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
         self.fc = nn.Linear(hidden_dim, vocab_size)
